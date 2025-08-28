2250aa6c3644a752c9a1378630688daee20b10a1
Hridaey Raya <hridaeyraya.22@gmail.com>
2025-08-28 10:31:37 +0545

    About section updated

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,6 +4,30 @@ const ContactPage = () => {
     return(
         <div className="">
             {/* Hero Section */}
+            <section className="">
+                <h1 className="">Contact SleepTracker</h1>
+                <p className="">
+                    Have question or need help > GEt in touch !
+                </p>
+            </section>
+
+            {/* Contact form Section */}
+            <section className="">
+                <h2 className="">
+                    Get in touch
+                </h2>
+                <form className="" onSubmit={(e)}>
+                    e.preventDefault()
+                    const name = (document.getElementById('name') as HTMLInputElement)
+                    ?.value;
+                    const email = (document.getElementById('email') as HTMLInputElement)?<div className="value">
+                        const message =(
+                            document.getElementById('message') as HTMLTextAreaElement
+                        )?.value
+                        const mailtoLink = `mailto:support@sleepmonitor.com?subject=Message from ${name}&body=Email: ${}`
+                    </div>
+                </form>
+            </section>
         </div>
     )
 }
