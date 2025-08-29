39da781739f7760b455b7ad270dd792f3119e758
Hridaey Raya <hridaeyraya.22@gmail.com>
2025-08-29 20:17:24 +0545

    user account creation

---
+++ app/actions/addSleepRecord.tsx
@@ -0,0 +1,12 @@
+'use server'
+import React from 'react'
+
+const addSleepRecord = () => {
+  return (
+    <div>
+       
+    </div>
+  )
+}
+
+export default addSleepRecord
--- components/AddNewRecord.tsx
+++ components/AddNewRecord.tsx
@@ -1,8 +1,24 @@
 "use client"
-import React from 'react'
+import React, { useReducer, useRef, useState } from 'react'
+import addSleepRecord from '@/app/actions/addSleepRecord'
 
 const AddNewRecord = () => {
-    
+    const formRef = useRef<HTMLFormElement>(null)
+    const [amount, setAmount] = useState(6)
+    const [alertMessage, setAlertMessage] = useState<string | null>(null)
+    const [alertType, setAlertType] = useState<'success' | 'error' | null>(null)
+    const [isLoading, setIsLoading] = useState(false)
+    const [sleepQuality, setSleepQuality] = useState('')
+
+    const clientAction = async (formData:FormData) => {
+        setIsLoading(true)
+        setAlertMessage(null)
+
+        formData.set('amount', amount.toString());
+        formData.set('text', sleepQuality)
+
+        const {error} = await addSleepRecord(formData)
+    }
   return (
     <div>
       
