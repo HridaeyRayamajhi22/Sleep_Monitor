e66b43628fea674ad80ec6372f57a56e0360378c
Hridaey Raya <hridaeyraya.22@gmail.com>
2025-09-01 14:25:39 +0545

    Db error fixed

---
+++ app/actions/getUserRecord.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class getUserRecord extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default getUserRecord
---
+++ components/RecordChart.tsx
@@ -0,0 +1,4 @@
+import getRecords from "@/app/actions/getRecords";
+import { BarChart } from "lucide-react";
+
+const RecordChart
